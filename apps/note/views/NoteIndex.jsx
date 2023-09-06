126d81f6df0407566bd510ba746cd1c3999d56ff
Carmel Amarilio <carmelamarilio@gmail.com>
2023-09-06 15:39:04 +0300

    solve conflict

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,41 @@
+const { useState, useEffect } = React;
+import { noteService } from "../services/note.service.js";
+import { NoteList } from "../cmps/NoteList.jsx";
 
 export function NoteIndex() {
-    return <div>note app</div>
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    noteService.query().then((res) => {
+      console.log(res);
+      setNotes(res);
+    });
+  }, []);
+
+  function onRemoveNote(noteId) {
+    noteService
+      .remove(noteId)
+      .then(() => {
+        setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+        // showSuccessMsg(`Book Removed! ${bookId}`);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+        // showErrorMsg("Problem Removing " + bookId);
+      });
+  }
+
+  if (!notes) return <div>Loading...</div>;
+  return (
+    <div>
+      <div className={"noteIndex"}>
+        <div className={"note-header flex"}>
+          <h1>Appsus Keep</h1>
+          <input type="text" />
+          <button>search</button>
+        </div>
+        <NoteList notes={notes} onRemoveNote={onRemoveNote}></NoteList>
+      </div>
+    </div>
+  );
 }
