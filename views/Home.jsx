864e3ac98dc4110e413e30f8ffe15a2d01e88e9d
AlmogJan <Almogj1998@gmail.com>
2023-09-09 22:23:11 +0300

    homepage

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,10 @@
-
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
+  return (
+    <section className={"home flex column justify-center align-center"}>
+      <div className={"flex column justify-center align-center"}>
+        <h1>Ride the Innovation Wave with Appsus </h1>
+        <h2>Where Technology Gallops Ahead</h2>
+      </div>
     </section>
+  );
 }
